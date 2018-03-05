0a8a89a6296ed32f0fbc59632dd22c6a2cec3635
Rounak Vyas <itsron143@gmail.com>
2018-03-06 01:27:44 +0530

    Add files via upload

---
+++ style_transfer.py
@@ -0,0 +1,190 @@
+"""Generating personalised images using Neural Style Transfer"""
+# Importing all the required packages
+from __future__ import print_function
+from PIL import Image
+import time
+import numpy as np
+import argparse
+
+from keras import backend
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+
+from scipy.optimize import fmin_l_bfgs_b
+from scipy.misc import imsave
+
+# Dimensions of the images
+height = 512
+width = 512
+
+# Passing image addresses as arguments
+parser = argparse.ArgumentParser(description="Neural Style Transfer with Keras.")
+parser.add_argument('content_image_path', metavar='base', type=str, help='Path to the Content Image')
+parser.add_argument('style_image_path', metavar='ref',
+                    type=str, help='Path to the Style Image')
+args = parser.parse_args()
+# Import Images and Resize them
+content_image_path = args.content_image_path
+content_image = Image.open(content_image_path)
+content_image = content_image.resize((width, height))
+
+style_image_path = args.style_image_path
+style_image = Image.open(style_image_path)
+style_image = style_image.resize((width, height))
+
+content_array = np.asarray(content_image, dtype='float32')
+content_array = np.expand_dims(content_array, axis=0)
+
+style_array = np.asarray(style_image, dtype='float32')
+style_array = np.expand_dims(style_array, axis=0)
+
+content_array[:, :, :, 0] -= 103.939
+content_array[:, :, :, 1] -= 116.779
+content_array[:, :, :, 2] -= 123.68
+content_array = content_array[:, :, :, ::-1]
+
+style_array[:, :, :, 0] -= 103.939
+style_array[:, :, :, 1] -= 116.779
+style_array[:, :, :, 2] -= 123.68
+style_array = style_array[:, :, :, ::-1]
+
+content_image = backend.variable(content_array)
+style_image = backend.variable(style_array)
+combination_image = backend.placeholder((1, height, width, 3))
+
+input_tensor = backend.concatenate([content_image, style_image, combination_image], axis=0)
+
+model = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=False)
+layers = dict([(layer.name, layer.output) for layer in model.layers])
+# print(layers)
+
+content_weight = 0.025
+style_weight = 9.0
+total_variation_weight = 1.0
+loss = backend.variable(0.)
+
+# Content Loss
+
+
+def content_loss(content, combinaton):
+    return backend.sum(backend.square(combinaton - content))
+
+
+layer_features = layers['block2_conv2']
+content_image_features = layer_features[0, :, :, :]
+combination_features = layer_features[2, :, :, :]
+# Content loss appended to the loss variable
+loss += content_weight * content_loss(content_image_features,
+                                      combination_features)
+
+# Style Loss
+
+
+def gram_matrix(x):
+    features = backend.batch_flatten(backend.permute_dimensions(x, (2, 0, 1)))
+    gram = backend.dot(features, backend.transpose(features))
+    return gram
+
+
+def style_loss(style, combinaton):
+    S = gram_matrix(style)
+    C = gram_matrix(combinaton)
+    channels = 3
+    size = width * height
+    return backend.sum(backend.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+
+
+feature_layers = ['block1_conv2', 'block2_conv2', 'block3_conv3', 'block4_conv3', 'block5_conv3']
+
+for layer_name in feature_layers:
+    layer_features = layers[layer_name]
+    style_features = layer_features[1, :, :, :]
+    combination_features = layer_features[2, :, :, :]
+    sl = style_loss(style_features, combination_features)
+    loss += (style_weight / len(feature_layers)) * sl
+
+# Total Variation Loss (Content + Style)
+
+
+def total_variation_loss(x):
+    a = backend.square(x[:, :height - 1, :width - 1, :] - x[:, 1:, :width - 1, :])
+    b = backend.square(x[:, :height - 1, :width - 1, :] - x[:, :height - 1, 1:, :])
+    return backend.sum(backend.pow(a + b, 1.25))
+
+
+loss += total_variation_weight * total_variation_loss(combination_image)
+
+# Solving the optimisation problem
+grads = backend.gradients(loss, combination_image)
+
+outputs = [loss]
+outputs += grads
+f_outputs = backend.function([combination_image], outputs)
+
+
+def eval_loss_and_grads(x):
+    x = x.reshape((1, height, width, 3))
+    outs = f_outputs([x])
+    loss_value = outs[0]
+    grad_values = outs[1].flatten().astype('float64')
+    return loss_value, grad_values
+
+
+class Evaluator(object):
+
+    def __init__(self):
+        self.loss_value = None
+        self.grads_values = None
+
+    def loss(self, x):
+        assert self.loss_value is None
+        loss_value, grad_values = eval_loss_and_grads(x)
+        self.loss_value = loss_value
+        self.grad_values = grad_values
+        return self.loss_value
+
+    def grads(self, x):
+        assert self.loss_value is not None
+        grad_values = np.copy(self.grad_values)
+        self.loss_value = None
+        self.grad_values = None
+        return grad_values
+
+
+evaluator = Evaluator()
+
+x = np.random.uniform(0, 255, (1, height, width, 3)) - 128.
+
+iterations = 101
+
+for i in range(iterations):
+    print('Start of iteration', i)
+    start_time = time.time()
+    x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
+                                     fprime=evaluator.grads, maxfun=20)
+    print('Current loss value:', min_val)
+    end_time = time.time()
+    print('Iteration %d completed in %ds' % (i, end_time - start_time))
+    if i % 10 == 0:
+        x = x.reshape((height, width, 3))
+        x = x[:, :, ::-1]
+        x[:, :, 0] += 103.939
+        x[:, :, 1] += 116.779
+        x[:, :, 2] += 123.68
+        x = np.clip(x, 0, 255).astype('uint8')
+        img = Image.fromarray(x)
+        img.save('Results/result_%d.png' % (i))
+
+
+
+# Deprocess the image
+"""x = x.reshape((height, width, 3))
+x = x[:, :, ::-1]
+x[:, :, 0] += 103.939
+x[:, :, 1] += 116.779
+x[:, :, 2] += 123.68
+x = np.clip(x, 0, 255).astype('uint8')
+
+imgs = Image.fromarray(x)
+imgs.save("Results/result.png")
+imgs.show()"""
